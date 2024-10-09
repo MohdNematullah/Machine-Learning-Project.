0b682476f1e7567bab15ad474db1990aee7f11eb
Mohd Nematullah <156227098+MohdNematullah@users.noreply.github.com>
2024-10-10 04:32:39 +0530

    Update logger.py

--- src/Pipeline/logger.py
+++ src/Pipeline/logger.py
@@ -1,1 +1,15 @@
 import logging
+import os
+from datetime import datetime
+
+Log_File = f"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log"
+Log_Path = os.path.join(os.getcwd(),"logs",Log_File)
+os.makedirs(Log_Path,exist_ok=True)
+
+Log_File_Path = os.path.join(Log_File,Log_Path)
+
+logging.basicConfig(
+    filename = Log_File_Path,
+    format = "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
